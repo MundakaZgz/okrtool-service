d8ce7a0587d1412e41e923ccbee271538daf4b39
MundakaZgz <j.herrero.moriana@gmail.com>
2022-01-13 14:13:51 +0100

    Initial commit

---
+++ src/main/kotlin/com/mundaka/okrtool/OkrtoolApplication.kt
@@ -0,0 +1,11 @@
+package com.mundaka.okrtool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OkrtoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<OkrtoolApplication>(*args)
+}
---
+++ src/test/kotlin/com/mundaka/okrtool/OkrtoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mundaka.okrtool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OkrtoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
